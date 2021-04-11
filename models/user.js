e73b9fcc64efc1dc1902329343b98b90d6569a5b
jamieroszel <jamie.s.roszel@gmail.com>
2021-04-10 22:39:35 -0400

    create user model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+// Import Schema and Model
+const { Schema, model } = require("../db/connection.js")
+
+// The Podcast Schema
+const Podcast = new Schema({
+  text: String,
+})
+
+// The User Schema
+const UserSchema = new Schema(
+  {
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    // The podcast property defined as an array of objects that match the Podcast schema
+    podcasts: [Podcast],
+  },
+  { timestamps: true }
+)
+
+// The User Model
+const User = model("User", UserSchema)
+
+// Export the User Model
+module.exports = User
